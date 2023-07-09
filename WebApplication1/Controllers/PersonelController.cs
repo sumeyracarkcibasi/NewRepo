0de53c07e2f14a516a89b7317bec2c46cc7a7c4b
Sümeyra Çarkçıbaşı <sumeyra_carkcibasi20@erdogan.edu.tr>
2023-07-09 10:57:39 +0300

    deneme

---
+++ WebApplication1/Controllers/PersonelController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PersonelController : Controller
+    {
+        public IActionResult Index()
+        {
+            Personel p1 = new Personel() { Ad="Selami", Soyad = "Şahin", Departman = "Müzik", Aktif = true };
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Personel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Personel
+    {
+        [Display(Name ="Personel Adı")]
+        [StringLength(50,ErrorMessage ="Bu alana en fazla 50 karakter yazılabilir.")]
+        public string Ad { get; set; }
+
+        [DisplayName("Personel Soyadı")]
+        public string Soyad { get; set; }
+        public string Departman { get; set; }
+        public bool Aktif { get; set; }
+
+    }
+}
